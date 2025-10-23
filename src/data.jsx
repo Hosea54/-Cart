78c71ee089a0d1edabc0720d2471d3939f220358
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-10-23 15:07:46 -0400

    data setup

---
+++ src/data.jsx
@@ -0,0 +1,12 @@
+import img1 from "./images/Iphone.jpg";
+import img2 from "./images/Iphone.jpg";
+import img3 from "./images/Iphone.jpg";
+import img4 from "./images/Iphone.jpg";
+
+const cartItems = [
+  { id: "rec1JZlfCIBOPdcT2", title: "Iphone", price: "899.99", img: img1 },
+  { id: "recB6qcHPxb62YJ75", title: "MacBook", price: "2599.99", img: img2 },
+  { id: "recdRxBsE14Rr2VuJ", title: "Shoes", price: "89.99", img: img3 },
+  { id: "recwTo160XST3PIoW", title: "Watch", price: "890.99", img: img4 },
+];
+export default cartItems;
